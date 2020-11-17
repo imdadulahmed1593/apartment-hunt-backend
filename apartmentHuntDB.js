3339eef2b9ea6ae9d07c5bef0dd20fcd66d6f51a
IMDAD <ahmedimdad1593@gmail.com>
2020-11-17 20:43:11 +0600

    initial commit

---
+++ apartmentHuntDB.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+// const serviceSchema = mongoose.Schema({
+//   icon: String,
+//   title: String,
+//   description: String,
+//   url: String,
+// });
+
+// const reviewSchema = mongoose.Schema({
+//   img: String,
+//   name: String,
+//   title: String,
+//   description: String,
+// });
+
+const bookingSchema = mongoose.Schema({
+  name: String,
+  emailID: String,
+  phoneNo: String,
+  message: String,
+  status: String,
+  houseName: String,
+  price: String,
+});
+
+// const serviceCollection = mongoose.model("serviceCollection", serviceSchema);
+
+// const reviewCollection = mongoose.model("reviewCollection", reviewSchema);
+
+const bookingCollection = mongoose.model("bookingCollection", bookingSchema);
+
+export { bookingCollection };
